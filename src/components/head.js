a3a8152c9a1ecc8429bafae0ff4d06545a1206f5
Oribe <d_egoist@163.com>
2017-09-29 18:07:02 +0800

    项目初建

---
+++ src/components/head.js
@@ -0,0 +1,12 @@
+'use strict'
+import React, {component} from 'react'
+
+class Header extends component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Header;
